aa7a8cca3891a86348d0ca0afb35da16bd46eb39
张福龙 <zhangfulong@zhangfuongdeAir.lan>
2020-05-10 20:50:30 +0800

    Initial commit

---
+++ config/base_setting.py
@@ -0,0 +1,19 @@
+SERVER_PORT = 8999
+DEBUG = False
+SQLALCHEMY_ECHO = False
+
+AUTH_COOKIE_NAME= "mooc_food"
+
+#过滤URL
+IGNORE_URLS = [
+    "^/user/login"
+]
+
+IGNORE_CHECK_LOGIN_URLS = [
+    "^/static",
+    "^/favicon.ico"
+]
+
+PAGE_SIZE = 50
+
+PAGE_DISPLAY = 10
---
+++ reademe.md
@@ -0,0 +1,2 @@
+## 启动
+* export ops_config=local|production && pytho manager.py runserver
